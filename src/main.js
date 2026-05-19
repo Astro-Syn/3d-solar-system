d8bf68b3832b03271b12aba647f5289fdcd62404
Astro-Syn <kelseyamyb@gmail.com>
2026-05-19 15:51:33 -0400

    more practice

--- src/main.js
+++ src/main.js
@@ -4,24 +4,30 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "green"})
-
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "green", wireframe: true})
 const cubeMesh = new THREE.Mesh(
   cubeGeometry, 
   cubeMaterial
-)
+);
+
+
+
 scene.add(cubeMesh)
 
-cubeMesh.position.y = 1;
+
 
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
 
+cubeMesh.scale.y = 1;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
-  0.5,
-  400)
+
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight,
+  0.1,
+  200);
+  
+  cubeMesh.position.distanceTo(camera.position)
 
 
 const aspectRatio = window.innerWidth / window.innerHeight
